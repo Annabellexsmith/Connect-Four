5d4df3caba546beeaf335b4b135542d785fda281
AnnabellexSmith <sannabelle056@gmail.com>
2024-04-25 08:28:13 -0700

    cleaning up code

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -4,15 +4,15 @@ let player;
 let winner;
 
 // CONSTANTS --------------------------------------------------------
- const winningCombos = [
- [0, 1, 2, 3], [1, 2, 3, 4], [5, 6, 7, 8], [6, 7, 8, 9],
- [10, 11, 12, 13], [11, 12, 13, 14], [15, 16, 17, 18], [16, 17, 18, 19],
- [20, 21, 22, 23], [21, 22, 23, 24], [0, 5, 10, 15, ], [5, 10, 15, 20],
- [1, 6, 11, 16], [6, 11, 16, 21],[2, 7, 12, 17],  [7, 12, 17, 22],
- [3, 8, 13, 18], [8, 13, 18, 23], [4, 9, 14, 19], [9, 14, 19, 24], 
- [3, 7, 11, 15], [4, 8, 12, 16], [8, 12, 16, 20], [21, 17, 13, 9],
-[1, 7, 13, 19], [0, 6, 12, 18], [6, 12, 18, 24], [5, 11, 17, 23]
-]
+const winningCombos = [
+    [0, 1, 2, 3], [1, 2, 3, 4], [5, 6, 7, 8], [6, 7, 8, 9],
+    [10, 11, 12, 13], [11, 12, 13, 14], [15, 16, 17, 18], [16, 17, 18, 19],
+    [20, 21, 22, 23], [21, 22, 23, 24], [0, 5, 10, 15, ], [5, 10, 15, 20],
+    [1, 6, 11, 16], [6, 11, 16, 21], [2, 7, 12, 17], [7, 12, 17, 22],
+    [3, 8, 13, 18], [8, 13, 18, 23], [4, 9, 14, 19], [9, 14, 19, 24], 
+    [3, 7, 11, 15], [4, 8, 12, 16], [8, 12, 16, 20], [21, 17, 13, 9],
+    [1, 7, 13, 19], [0, 6, 12, 18], [6, 12, 18, 24], [5, 11, 17, 23]
+];
 
 const LOOKUP = {
     "1": "Player One",
@@ -39,7 +39,7 @@ const restartButtonEl = document.getElementById('restart');
 playButtonEl.addEventListener("click", function () {instructionEl.close()});
 slotEls.forEach(function (slotEl) {slotEl.addEventListener("click", dropToken)});
 tryAgainButtonEl.addEventListener("click", function () {alertDisplayEl.close()});
-restartButtonEl.addEventListener("click", function() {resultDisplayEl.close(); init()});
+restartButtonEl.addEventListener("click", function() {resultDisplayEl.close(), init()});
 
 // FUNCTIONS ---------------------------------------------------------
 function init () {
@@ -71,12 +71,11 @@ function dropToken(event) {
         event.target.alt = `This is ${LOOKUP[player]}'s token`;
         board[pickedSlot] = player;
         winner = assessForWinner();
-        if (winner === "win" || winner === "tie") {
-            updateMessage();
-            resultDisplayEl.showModal();
-            return;
-        } else if (winner === null) {
-        }
+            if (winner === "win" || winner === "tie") {
+                updateMessage();
+                resultDisplayEl.showModal();
+                return;
+            }
     } else {
         alertDisplayEl.showModal();
         return;
@@ -89,15 +88,15 @@ function assessForWinner () {
     for(let i = 0; i < winningCombos.length; i++) {
         if (Math.abs(board[winningCombos[i][0]] + board[winningCombos[i][1]]+ board[winningCombos[i][2]] + board[winningCombos[i][3]]) === 4) {
             return "win";
-        } 
-    } 
+        }; 
+    }; 
     if (!board.includes(null)) {
         return "tie";
-    }  
-}
+    }; 
+};
 
 function updateMessage() {
     if (winner === "win") resultMessageEl.textContent = `Congratulations ${LOOKUP[player]}! 
         You have won! Do you want to play again?`;
     else resultMessageEl.textContent = `Gasp! It's a tie. Only way to figure out who wins is to play again!`;
-}
+};
